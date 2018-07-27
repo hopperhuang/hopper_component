3bb18e3e99a8c96fb043af5f1121842b2ed7e29f
huangjiawei03 <huangjiawei03@maoyan.com>
2018-07-27 16:24:00 +0800

    1.Feat: changes to multi outputs

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,15 @@ import babel from 'rollup-plugin-babel';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+    }, {
+      file: 'dist/index.es.js',
+      format: 'es',
+    },
+  ],
   plugins: [
     babel({
       exclude: 'node_modules/**',
