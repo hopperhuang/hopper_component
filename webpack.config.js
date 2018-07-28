18bc26327017d114e8fa037e310d825c417ed5a7
hopperhuang <huanjiwe@126.com>
2018-07-28 12:15:23 +0800

    1.Feature: use clean-webpack-plugin to clean example dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// 暂时不使用此插件，否则会引起，每次重新编译时，dist文件夹中的html文件被删除，具体原因尚未明确
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -20,7 +19,7 @@ module.exports = {
     ],
   },
   plugins: [
-    // new CleanWebpackPlugin(['example/dist']),
+    new CleanWebpackPlugin(['example/dist']),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './example/src/index.html',
